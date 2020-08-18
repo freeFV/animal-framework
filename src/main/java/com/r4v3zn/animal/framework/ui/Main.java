9b08f3624eb4723715d023d4e69fb798465aade8
R4v3zn <woo0nise@gmail.com>
2020-08-19 01:10:20 +0800

    UI 构建

---
+++ src/main/java/com/r4v3zn/animal/framework/ui/Main.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2020. r4v3zn.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.r4v3zn.animal.framework.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+/**
+ * Title: Main
+ * Desc: TODO
+ * Date:2020/8/18 下午10:31
+ * Email:woo0nise@gmail.com
+ * Company:www.j2ee.app
+ *
+ * @author R4v3zn
+ * @version 1.0.0
+ */
+public class Main extends Application {
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = getClass().getClassLoader().getResource("fxml/Main.fxml");
+        assert url != null;
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("animal-framework");
+        primaryStage.setScene(new Scene(root, 850, 620));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
